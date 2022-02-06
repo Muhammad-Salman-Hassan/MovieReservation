c5f9dccb580208e9dec798d9de6e0272b04e4000
Muhammad-Salman-Hassan <msalmanhassan288311@gmail.com>
2022-02-06 10:57:11 +0500

    changes in index.js and adding some functions in this project

--- index.js
+++ index.js
@@ -3,18 +3,39 @@ const seats=document.querySelectorAll('.row .seat:not(.occupied)')
 let count=document.querySelector(".count")
 const total=document.querySelector(".total")
 const movieSelect=document.querySelector("#movies")
-const ticketPrice=+movieSelect.value
+let ticketPrice=+movieSelect.value
 // seats.forEach((seat)=>{
 //     seat.addEventListener("click",()=>{
 //         console.log("click")
 //     })
 // })
 
+const updateSelctedCount=()=>{
+    const selectedSeat=document.querySelectorAll(".row .seat.selected")
+    const seatIndex=[...selectedSeat].map((seat)=>{
+        return [...seats].indexOf(seat)
+    })
+    console.log(seatIndex)
+    const selectedSeatLength=selectedSeat.length
+    count.innerText=selectedSeatLength
+    total.innerText=selectedSeatLength*ticketPrice
+    localStorage.setItem("selectedSeats",JSON.stringify(seatIndex))
+}
+
+const setMovieUpdate=(index,MoviePrice)=>{
+localStorage.setItem("Selected Movie Price",MoviePrice)
+localStorage.setItem("Selected Movie Index",index)
+}
 
 container.addEventListener('click',(e)=>{
     if(e.target.classList.contains('seat')&&!e.target.classList.contains('occupied')){
         e.target.classList.toggle('selected')
+        updateSelctedCount()
     }
 })
-console.log(ticketPrice)
-console.log(container)
+
+movieSelect.addEventListener("change",(e)=>{
+    ticketPrice=Number(e.target.value)
+    setMovieUpdate(e.target.value,e.target.selectedIndex)
+    updateSelctedCount()
+})
